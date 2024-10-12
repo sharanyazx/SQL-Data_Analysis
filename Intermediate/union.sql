008f704ff875d747c0771fbd4b5cbe7e64ba4026
sharanya <116085931+sharanyazx@users.noreply.github.com>
2024-10-12 16:00:07 +0530

    Update union.sql

---
+++ Intermediate/union.sql
@@ -0,0 +1,67 @@
+SELECT first_name, last_name
+FROM employee_demographics
+UNION
+SELECT occupation, salary
+FROM employee_salary;
+
+
+
+SELECT first_name, last_name
+FROM employee_demographics
+UNION
+SELECT first_name, last_name
+FROM employee_salary;
+
+-- notice it gets rid of duplicates? Union is actually shorthand for Union Distinct
+
+SELECT first_name, last_name
+FROM employee_demographics
+UNION DISTINCT
+SELECT first_name, last_name
+FROM employee_salary;
+
+-- we can use UNION ALL to show all values
+
+SELECT first_name, last_name
+FROM employee_demographics
+UNION ALL
+SELECT first_name, last_name
+FROM employee_salary;
+
+
+
+#Now Let's actually try to use UNION
+# The Parks department is trying to cut their budget and wants to identify older employees they can push out or high paid employees who they can reduce pay or push out
+-- let's create some queries to help with this
+
+SELECT first_name, last_name, 'Old'
+FROM employee_demographics
+WHERE age > 50;
+
+
+
+SELECT first_name, last_name, 'Old Lady' as Label
+FROM employee_demographics
+WHERE age > 40 AND gender = 'Female'
+UNION
+SELECT first_name, last_name, 'Old Man'
+FROM employee_demographics
+WHERE age > 40 AND gender = 'Male'
+UNION
+SELECT first_name, last_name, 'Highly Paid Employee'
+FROM employee_salary
+WHERE salary >= 70000
+ORDER BY first_name
+;
+
+
+
+
+
+
+
+
+
+
+
+
